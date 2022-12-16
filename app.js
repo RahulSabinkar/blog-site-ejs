4837c33beb03703bc1ff8ffbfef84fcf591da9dc
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2022-12-16 18:36:57 +0530

    do challenge 1

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("home");
 })
 
 app.listen(PORT, function() {
